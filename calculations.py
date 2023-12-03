10b9aeba8b77d9aff69b6a8579fc5c931510f02d
lross2k <luisross2000@outlook.com>
2023-12-02 20:28:43 -0600

    Implemented all calculations for each iteration

--- calculations.py
+++ calculations.py
@@ -2,7 +2,7 @@ import openpyxl
 from typing import TypedDict, Sequence
 from collections import defaultdict
 from datetime import datetime, time
-from math import fsum, log, exp, cos, pi, sin, acos, tan
+from math import fsum, log, exp, cos, pi, sin, acos, tan, sqrt
 
 class SoilData(TypedDict):
     date:   list[datetime]
@@ -55,7 +55,7 @@ def load_data(file: str) -> SoilData:
         spreadsheet_data['PR'].append(float(row[6].value))
     return(spreadsheet_data)
 
-def calculate_velocity(vv_avg: float, measure_height_c: float) -> float:
+def calculate_wind_velocity(vv_avg: float, measure_height_c: float) -> float:
     """ Equation 47 speed of wind at 2m u_2 (m/2) """
     velocity = vv_avg*(4.87/(log((67.8*measure_height_c)-5.42)))
     return velocity
@@ -120,6 +120,39 @@ def calculate_extraterrestrial_radiation(solar_c: float, latitude_rad_c: float, 
     ra = ((24*60)/pi)*(solar_c*relative_distance)*((sunset*sin(latitude_rad_c)*sin(solar_declination))+(cos(latitude_rad_c)*cos(solar_declination)*sin(sun_middle_point)))
     return ra
 
+def calculate_max_duration(sunset: float) -> float:
+    """ Equation 34 """
+    max_duration = (24/pi)*sunset
+    return max_duration
+
+def calculate_r_so(height_c: float, extraterrestrial_radiation: float) -> float:
+    """ Equation 37 MJ/ m^(2)* """
+    r_so = (0.75+2*10**(-5)*height_c)*extraterrestrial_radiation
+    return r_so
+
+def calculate_radiations(albedo_c: float, steffan_c: float, solar_radiation: float, r_so: float, 
+                        ta_max: float, ta_min: float, p_real: float) -> dict[str, float]:
+    """ Equations 38, 39 and 40 """
+    short_wave_radiation = (1-albedo_c)*solar_radiation # MJ/ m^(2)*
+    relative_radiation = solar_radiation/r_so
+    long_wave_radiation = steffan_c*(((ta_max+273.16)+(ta_min+273.16))/2)*(0.34-0.14*sqrt(p_real))*((1.35*relative_radiation)-0.35) # MJ/ m^(2)*
+    net_radiation = short_wave_radiation - long_wave_radiation # MJ/ m^(2)*
+    return {'short_wave': short_wave_radiation, 'relative': relative_radiation, 'long_wave': long_wave_radiation, 'net': net_radiation}
+
+def calculate_soil_heat_flux(caloric_capacity_c: float, soil_depth_c: float, ta_avg: float, prev_ta_avg: float, amount_of_days: int) -> float:
+    """ Equations 45 and 46 MJ/ m^(2)* """
+    heat_flux = caloric_capacity_c*((ta_avg-prev_ta_avg)/(amount_of_days))*soil_depth_c
+    return heat_flux
+
+def calculate_evapotranspiration(saturation_slope: float, net_radiation: float, soil_heat_flux: float, 
+                                wind_velocity: float, ta_avg: float, steam_pressure_deficit: float, 
+                                psicrometric_c: float) -> float:
+    numerator_1 = 0.408*saturation_slope*(net_radiation-soil_heat_flux)
+    numerator_2 = psicrometric_c*(900/(ta_avg+273))*wind_velocity*(steam_pressure_deficit)
+    denominator = saturation_slope+(psicrometric_c*(1+0.34*wind_velocity))
+    evapotranspiration = (numerator_1+numerator_2)/denominator
+    return evapotranspiration
+
 def main() -> None:
     # Data obtained from file and processed
     data = load_data('test_data.xlsx')
@@ -135,6 +168,12 @@ def main() -> None:
     centre_logitude_deg_c = 90
     longitude_deg_c = 83.9
     solar_c = 0.082
+    height_c = 2129
+    albedo_c = 0.23
+    steffan_c = (4.903*10**(-9))/24
+    caloric_capacity_c = 2.1
+    soil_depth_c = 0.1
+    psicrometric_c = 0.05
 
     # Values defined by iterative loop
     year = 2019
@@ -143,34 +182,57 @@ def main() -> None:
 
     # Iterative loop procedure
     data = get_data_at(data, indexed_data[year][month][day])
+    prev_ta_avg = 0
+    amount_of_days = 0
+
+    amount_of_days += 1
 
     ta_values = values_for_variable(data['TA'])
-    print(ta_values)
     hr_values = values_for_variable(data['HR'])
-    print(hr_values)
     vv_values = values_for_variable(data['VV'])
-    print(vv_values)
     rs_values = values_for_variable(data['RS'])
-    print(rs_values)
-    print(values_for_variable(data['PR']))
 
-    print(calculate_velocity(vv_values['avg'], measure_height_c))
+    wind_velocity = calculate_wind_velocity(vv_values['avg'], measure_height_c)
     sat_steam = calculate_saturate_steam(ta_values['max'], ta_values['min'])
-    print(sat_steam)
-    print(calculate_saturate_slope(ta_values['avg']))
+    saturation_slope = calculate_saturate_slope(ta_values['avg'])
     p_real = calculate_real_steam_pressure(ta_values['min'], ta_values['max'], hr_values['min'], hr_values['max'])
-    print(p_real)
-    print(calculate_steam_pressure_deficit(sat_steam['avg_p'], p_real))
-    print(calculate_solar_radiation(rs_values['avg']))
+    steam_pressure_deficit = calculate_steam_pressure_deficit(sat_steam['avg_p'], p_real)
+    solar_radiation = calculate_solar_radiation(rs_values['avg'])
     julian_day = calculate_julian_day(month, day)
-    print(julian_day)
     relative_distance = calculate_relative_distance(julian_day)
-    print(relative_distance)
     solar_declination = calculate_solar_declination(julian_day)
-    print(solar_declination)
     hourly_radicion_angle = calculate_hourly_radicion_angle(julian_day, solar_declination, latitude_rad_c, max_point_c, centre_logitude_deg_c, longitude_deg_c)
+    extraterrestrial_radiation = calculate_extraterrestrial_radiation(solar_c, latitude_rad_c, relative_distance, solar_declination, hourly_radicion_angle['sunset'], hourly_radicion_angle['sun_middle_point'])
+    r_so = calculate_r_so(height_c, extraterrestrial_radiation) 
+    radiations = calculate_radiations(albedo_c, steffan_c, solar_radiation, r_so, ta_values['max'], ta_values['min'], p_real)
+    soil_heat_flux = calculate_soil_heat_flux(caloric_capacity_c, soil_depth_c, ta_values['avg'], prev_ta_avg, amount_of_days)
+    evapotranspiration = calculate_evapotranspiration(saturation_slope, radiations['net'], soil_heat_flux, wind_velocity, ta_values['avg'], steam_pressure_deficit, psicrometric_c)
+
+    print(ta_values)
+    print(hr_values)
+    print(vv_values)
+    print(rs_values)
+
+    print(values_for_variable(data['PR'])) # Never used for calculations
+
+    print(wind_velocity)
+    print(saturation_slope)
+    print(sat_steam)
+    print(p_real)
+    print(steam_pressure_deficit)
+    print(solar_radiation)
+    print(julian_day)
+    print(relative_distance)
+    print(solar_declination)
     print(hourly_radicion_angle)
-    print(calculate_extraterrestrial_radiation(solar_c, latitude_rad_c, relative_distance, solar_declination, hourly_radicion_angle['sunset'], hourly_radicion_angle['sun_middle_point']))
+    print(extraterrestrial_radiation)
+
+    print(calculate_max_duration(hourly_radicion_angle['sunset'])) # Never used for calculations
+
+    print(r_so)
+    print(radiations)
+    print(soil_heat_flux)
+    print(evapotranspiration)
 
 if __name__ == "__main__":
     main()
